627b8fc46b3b904efb1dd416c75262ceb7e2c63a
Poonam67-s <poonam.kalra@successive.tech>
2022-06-29 23:23:17 +0530

    added splash and logo

--- lib/constants/app_constants.dart
+++ lib/constants/app_constants.dart
@@ -48,4 +48,10 @@ class AppConstants
   static const separatedAddTaskString = "Add Task";
   static const jointAddTaskString = "AddTask";
   static const robotoTextStyleString = "Roboto";
+  static const successiveLogo = "assets/images/successive_logo.png";
+  static const marvelLogo = "assets/images/marvel_logo.png";
+  static const poweredBy = "Powered by :";
+  static const successive = "Successive";
+  static const technologies = "Technologies";
+  static const apps = "APPS";
 }
--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:realtime_database_app/constants/app_constants.dart';
 import 'package:realtime_database_app/screens/home_screen.dart';
+import 'package:realtime_database_app/screens/splash_screen.dart';
 
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
@@ -20,7 +21,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: HomeScreenPage(
+      home: SplashScreen(
       ),
     );
   }
